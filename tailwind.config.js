3c2722e636cc44671f02a5a6e783ff55b86def31
Khizar <khizar.ofc@gmail.com>
2025-04-16 14:28:10 +0500

    Setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          DEFAUlT: "#000",
+          100: "#000319",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
