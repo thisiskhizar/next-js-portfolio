3fc4087e1795818e46bbff719c2f64bc3d02838d
Khizar <khizar.ofc@gmail.com>
2025-04-23 20:17:27 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 // @ts-check
 
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: isProd ? '/next-js-portfolio/' : '',
+  basePath: isProd ? '/next-js-portfolio' : '',
   output: "export",
   typescript: {
     ignoreBuildErrors: true,
