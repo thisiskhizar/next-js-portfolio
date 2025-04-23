1d4a2afaceb9c35bc2ddfb76421be1731571ee4c
Khizar <khizar.ofc@gmail.com>
2025-04-23 12:49:24 +0500

    Setup navbar

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,22 @@ import MyApproachSection from "@/components/my-approach";
 import Projects from "@/components/projects";
 import Questions from "@/components/questions";
 import Testimonials from "@/components/testimonials";
-import { faqs } from "@/data";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { faqs, navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto px-5 sm:px-10">
       <div className="max-w-7xl w-full">
-        <Hero/>
-        <Grid/>
-        <Projects/>
-        <ExperienceSection/>
-        <MyApproachSection/>
-        <Testimonials/>
-        <Questions faqs={faqs}/>
-        <Footer/>
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <Projects />
+        <ExperienceSection />
+        <MyApproachSection />
+        <Testimonials />
+        <Questions faqs={faqs} />
+        <Footer />
       </div>
     </main>
   );
